b44d06595d205bfc4c501b7ae6cfb5ea92de85f5
Aoi-lucario <petersbw123@gmail.com>
2019-03-19 16:09:06 +0900

    Update mock_module.py

--- train_python/mock_module.py
+++ train_python/mock_module.py
@@ -1,14 +1,10 @@
-# python3
+try:
+    from unittest.mock import patch, mock_open  # python3
+except ImportError:
+    from mock import patch, mock_open  # python2
 
-"""
-# python2
-from mock import patch, mock_open
-import __builtin__
-"""
-
-from unittest.mock import patch, mock_open
 import builtins as __builtin__
-import subprocess
+import builtins
 import t2
 
 """
@@ -20,32 +16,34 @@ def run():
     with open('foo.sql', 'r') as f:
         print(f.read())
 
-if __name__ == '__main__':
-    run()
-
 # echo 'this is foo' > foo
 # echo 'this is foo.sql' > foo.sql
 """
 
 bi_open = __builtin__.open
 
-class LogsTestOpen(object):
-    def __init__(self, filename, mode='r'):
-        self.filename = filename
-        self.mode = mode
-        if filename.endswith('.sql'):
-            self._open = mock_open(read_data='this is mock_open()')
-        else:
-            self._open = bi_open
+def LogsTestOpen(filename, mode='r', buffering=-1, encoding=None, errors=None, newline=None, closefd=True):
+    if filename.endswith('.sql'):
+        _open = mock_open(read_data='this is mock_open()')
+    else:
+        _open = bi_open
 
-    def __enter__(self):
-        self.fd = self._open(self.filename, self.mode)
-        return self.fd
-
-    def __exit__(self, type, value, traceback):
-        self.fd.close()
-
+    return _open(filename, mode, buffering, encoding, errors, newline, closefd)
 
 with patch('builtins.open', LogsTestOpen, create=True):
     t2.run()
-    print(subprocess.check_output(['python3', 't2.py']).decode('utf-8'))
+
+"""
+$ python2.7 t.py 
+this is foo
+
+this is foo.sql
+
+$ python3.6 t.py 
+this is foo
+
+this is mock_open()
+
+
+# f * * k
+"""
