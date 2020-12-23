29831f2a31ce706a245df23c502bbf585106b77d
aoi-lucario <petersbw123@gmail.com>
2020-12-23 19:34:19 +0900

    Rewrite transcode.py

--- utils/transcode.py
+++ utils/transcode.py
@@ -1,98 +1,89 @@
-# encode: utf-8
+# -*- coding: utf-8 -*-
 # python3
-# windows only
+
+"""
+Convert encoding of filename and content. Use for windows.
+
+Usage:
+python3 transcode.py  -->  convert SHIST-JIS to GBK from current path
+"""
 
 import os
-import sys
+import logging
+from os.path import isfile, isdir, join
 from optparse import OptionParser
 
 # parser
 parser = OptionParser()
-parser.add_option("-f", "--filename",
-                  action="store_true", dest="f", default=True,
-                  help="transcode filename? | default: true")
-                  
-parser.add_option("-F", "--foldername",
-                  action="store_true", dest="F", default=True,
-                  help="transcode foldername? | default: true")
-
-parser.add_option("-c", "--content",
-                  action="store_true", dest="c", default=False,
-                  help="transcode file content? | default: false")
-
-parser.add_option("-e", "--extend",
-                  dest="e", default='txt',
-                  help="extends list, split by ',' | default: txt")
-     
 parser.add_option("-p", "--path",
                   dest="p", default='.',
                   help="path | default: .")
 
-### gb18030: gb2312 + gbk -> gb18030. windows only ###
+parser.add_option("-e", "--extend",
+                  dest="e", default='txt',
+                  help="extends list, split by ',' | default: txt")
+
+# gb18030: gb2312 + gbk -> gb18030. cn windows
 parser.add_option("-i", "--input",
                   dest="i", default='gb18030',
-                  help="input code | default: gb18030")
+                  help="input encoding | default: gb18030")
 
-### cp932: update version of shift-jis ###
+# cp932: update version of shift-jis. jp windows
 parser.add_option("-o", "--output",
                   dest="o", default='cp932',
-                  help="output code | default: cp932")
-
-options, args = parser.parse_args()
-path = options.p
-in_code = options.i
-out_code = options.o
-extends = options.e
-while True:
-    if extends.startswith('"'):
-        extends = extends[1:]
-    elif extends.endswith('"'):
-        extends = extends[:-1]
-    else:
-        break
+                  help="output encoding | default: cp932")
 
 #############################################
-# folder
-if options.F:
-    print("\n----------\ntrans folders:\n----------\n")
-    for i in os.walk(path):
-        root, folders, files = i
-        print("walk in: %s" % root)
-        for folder in folders:
-            full_path =  "%s\%s" % (root, folder)
-            new_folder = folder.encode(in_code).decode(out_code)
-            os.rename(full_path, "%s\%s" % (root, new_folder))
-            try:
-                print("  - %s ------> %s" % (folder, new_folder))
-            except:
-                pass
 
-# file
-if options.f:
-    print("\n----------\ntrans files:\n----------\n")
-    for i in os.walk(path):
-        root, folders, files = i
-        print("walk in: %s" % root)
-        for file in files:
-            full_path =  "%s\%s" % (root, file)
-            new_file = file.encode(in_code).decode(out_code)
-            os.rename(full_path, "%s\%s" % (root, new_file))
-            try:
-                print("  - %s ------> %s" % (file, new_file))
-            except:
-                pass
-            
-# content
-if options.c:
-    print("\n----------\ntrans content:\n----------\n")
-    for i in os.walk(path):
-        root, folders, files = i
-        print("walk in: %s" % root)
-        for file in files:
-            full_path =  "%s\%s" % (root, file)
-            with open(full_path, 'rb') as f:
-                origin_c = f.read
-            trans_c = origin_c.decode(in_code).encode(out_code)
-            with open(full_path, 'w') as f:
-                f.write(trans_c)
-            print("  - trans contets: file")
+
+class Converter:
+    in_code = ''
+    out_code = ''
+
+    def __init__(self, in_code, out_code, extensions):
+        self.in_code = in_code
+        self.out_code = out_code
+        self.extensions = extensions
+
+    def convert(self, path):
+        path = os.path.abspath(path)
+        if not isdir(path):
+            raise ValueError('Path: {} is not a directory!'.format(path))
+        for re_path in os.listdir(path):
+            target = join(path, re_path)
+            if any([target.endswith(ext) for ext in self.extensions]):
+                self.convert_content(target)
+            if isfile(target):
+                self.convert_filename(target)
+            if isdir(target):
+                self.convert(target)
+        self.convert_filename(path)
+
+    def convert_filename(self, path: str):
+        dirname = os.path.dirname(path)
+        basename = os.path.basename(path)
+        try:
+            new_name = basename.encode(self.in_code).decode(self.out_code)
+            os.rename(path, join(dirname, new_name))
+            logging.debug('{} -> {}'.format(basename, new_name))
+        except UnicodeDecodeError:
+            logging.error('Can not convert file: {}'.format(path))
+
+    def convert_content(self, path: str):
+        try:
+            with open(path, 'rb') as f:
+                text_b = f.read().decode(self.out_code)
+            with open(path, 'wb') as f:
+                f.write(text_b.encode('utf-8'))
+        except UnicodeDecodeError:
+            logging.error('Can not convert content in: {}'.format(path))
+
+
+if __name__ == '__main__':
+    options, args = parser.parse_args()
+    converter = Converter(
+        in_code=options.i,
+        out_code=options.o,
+        extensions=options.e
+    )
+    converter.convert(options.p)
